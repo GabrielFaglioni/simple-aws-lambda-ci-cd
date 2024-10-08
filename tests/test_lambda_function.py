a7579bba28121dddde738544baa65a9a4d1ca976
Gabriel Faglioni Mendes <gabriel.faglioni@gmail.com>
2024-10-08 15:37:48 -0300

    'Fixing' the wrong function

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -4,12 +4,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import json
 from src.lambda_function import lambda_handler
 
-@pytest.fixture
-def capture_stdout(capsys):
-    yield capsys
-
 def test_lambda_function():
   assert {
     'statusCode': 200,
-    'body': json.dumps('Last test')
+    'body': json.dumps('Testing successul')
   } == lambda_handler(None, None)
